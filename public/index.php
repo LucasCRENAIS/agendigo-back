02fa6cb94681b07879221a886106c5d3eb8452e1
Lucas <lucas.crenais@gmail.com>
2022-05-29 18:34:53 +0200

    Nginx Procfile and config

--- public/index.php
+++ public/index.php
@@ -1,29 +1,29 @@
 <?php
-//
-//use App\Kernel;
-//use Symfony\Component\Dotenv\Dotenv;
-//use Symfony\Component\ErrorHandler\Debug;
-//use Symfony\Component\HttpFoundation\Request;
-//
-//require dirname(__DIR__).'/vendor/autoload.php';
-//
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
 //(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-//
+
 //if ($_SERVER['APP_DEBUG']) {
 //    umask(0000);
 //
 //    Debug::enable();
 //}
-//
+
 //$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 //$request = Request::createFromGlobals();
 //$response = $kernel->handle($request);
 //$response->send();
 //$kernel->terminate($request, $response);
-//
-//$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
-//$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-//if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
-//if($trustedProxies) {
-//    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
-//}
+
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+if($trustedProxies) {
+    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
+}
