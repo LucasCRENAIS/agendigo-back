940fa9f714bfb84cb215e7b7a6bb0a2a929ca2e2
Lucas <lucas.crenais@gmail.com>
2022-05-30 00:58:22 +0200

    heroku config

--- public/index.php
+++ public/index.php
@@ -20,9 +20,8 @@ $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
 
-//$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
-//$trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-//if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
-//if($trustedProxies) {
-//    Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
-//}
+
+if((!empty( $_SERVER['HTTP_X_FORWARDED_HOST'])) || (!empty( $_SERVER['HTTP_X_FORWARDED_FOR'])) ) {
+    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    $_SERVER['HTTPS'] = 'on';
+}
